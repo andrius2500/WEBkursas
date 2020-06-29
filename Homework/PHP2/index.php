2a593ab821a4126a5c10aa8cb22ad77987f6a26d
Andrius <andrius2500@gmail.com>
2020-06-29 21:25:44 +0300

    2 PHP namų darbas

---
+++ Homework/PHP2/index.php
@@ -0,0 +1,23 @@
+<?php
+	$pageTitle = 'PHP2';
+	$country = 'Lietuva';
+	$city = 'Vilnius';
+
+	echo $city . ' - Lietuvos sostinė.';
+	echo $city . ' ir ' . $country . ' sparčiai auga.'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title><?php echo $pageTitle ?></title>
+	</head>
+
+	<body>
+
+	</body>
+</html>
+
+
