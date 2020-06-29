f40396793d8d0df4383d2a3eda65fe4e84de40a5
Andrius <andrius2500@gmail.com>
2020-06-29 21:15:58 +0300

    1 PHP namų darbas

---
+++ Homework/PHP1/index.php
@@ -0,0 +1,23 @@
+<?php
+	$country = 'Lietuva';
+	$city = 'Vilnius';
+	$population = 574221;
+
+?>
+<ul>
+	<li>
+		<?php
+			echo $country;
+		?>
+	</li>
+	<li>
+		<?php
+			echo $city;
+		?>
+	</li>
+	<li>
+		<?php
+			echo $population;
+		?>
+	</li>
+</ul>
