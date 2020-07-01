e87b8d39b480d0557fba56b57a817c24320f565a
Andrius <andrius2500@gmail.com>
2020-07-01 21:15:34 +0300

    PHP 10 namų darbas

---
+++ Homework/PHP10/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function rectangleCorners($firstCorner, $secondCorner = '0')
+{
+	$result = $firstCorner * $secondCorner;
+	return $result;
+}
+echo rectangleCorners(5, 2);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP11</title>
+	</head>
+
+	<body>
+
+	</body>
+</html>
