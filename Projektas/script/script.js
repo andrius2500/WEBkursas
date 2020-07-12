6269b6ba8ac3bf42a4627a3a7b146a9ae561850e
Andrius <andrius2500@gmail.com>
2020-07-12 20:36:30 +0300

    Projektas

--- Projektas/script/script.js
+++ Projektas/script/script.js
@@ -10,12 +10,30 @@ function menuSlide() {
 menuSlide();
 
 
-/*function headerSlide() {
-	var headerImage = document.querySelector('.ollin-background');
+// carousel slideshow
+const carouselSlide = document.querySelector('.image-container');
+const carouselImages = document.querySelectorAll('.image-container img');
 
-	window.addEventListener('scroll', function()
-	{
-		headerImage.style.backgroundPosition = +window.pageYOffset + "px";
-	});
-}
-headerSlide();*/
+//carousel buttons
+const previousButton = document.querySelector('#previous-button');
+const nextButton = document.querySelector('#next-button');
+
+// counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+//button listeners
+nextButton.addEventListener('click',()=> {
+	if (counter >= carouselImages.length - 1) return;
+	carouselSlide.style.transition = "transform 0.4s ease-in-out";
+	counter++;
+	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+previousButton.addEventListener('click',()=> {
+	if (counter <= 0) return;
+	carouselSlide.style.transition = "transform 0.4s ease-in-out";
+	counter--;
+	carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
